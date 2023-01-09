f1795ae012ed0e2d5bdc44c94ad8e3389f370ffc
Anusha <arshanapallyanusha@gmail.com>
2023-01-09 18:40:34 +0530

    THIRD COMMIT

--- src/main/java/com/app/controller/StudentController.java
+++ src/main/java/com/app/controller/StudentController.java
@@ -9,7 +9,11 @@ public class StudentController {
   @RequestMapping("")
   public String createStudent()
   {
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Anusree1772/SpringBootRestTemplateConsumerMiniProject
      int i=500;
+=======
+     int i=1000;
+>>>>>>> 05568e0 THIRD COMMIT
 	  return null;
   }
 }
